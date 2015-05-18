80aef903e98a42202c02f0d27b1c19ff356e5179
杜蒙 <dumeng@wangwenengdeAir.lan>
2015-05-18 09:30:18 +0800

    Initial Commit

---
+++ design IM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  design IM
+//
+//  Created by 杜蒙 on 15/5/18.
+//  Copyright (c) 2015年 杜蒙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ design IM/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  design IM
+//
+//  Created by 杜蒙 on 15/5/18.
+//  Copyright (c) 2015年 杜蒙. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
